c89817f3471f58a731ee88035be74700125c36eb
andrelbastida <andrelbastida@yahoo.com.br>
2023-11-22 14:14:08 -0300

    teste/erro

---
+++ alfabit-design-system/src/components/Typography/Typography.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+
+export type TypographyProps= { 
+    variant?: 'primary' | 'secondary' | 'tertiary'; 
+    size?: 'xs' | 'sm' | 'md' | 'xl' | 'title1' | 'title2' | 'title3';
+    children: React.ReactNode;
+    element?: keyof JSX.IntrinsicElements;
+} & JSX.IntrinsicElements["p"];
+
+const variantClassMap = { 
+    variant : {
+        primary: "text-gray-primary",
+        secondary: "text-gray-secondary",
+        tertiary: "text-gray-tertiary",
+    },
+    size :{
+        xs: "text-xs",
+        sm: "text-sm",
+        md: "text-md",
+        xl: "text-xl",
+        title1: "text-tx1",
+        title2: "text-tlg",
+        title3: "text-tmd",
+    },
+}
+
+const Typography = ({variant= 'primary', size='md', children, element='p', ...rest}: TypographProps) =>{
+    const Element= element as any;
+    return(
+        <div>
+            <Element className={`text-gray-primary text-sm font-normal ${variantClassMap.variant[variant]} ${variantClassMap.size[size]}`} {...rest}>{children}</Element>
+        </div>
+    )
+}
+
+export default Typography
